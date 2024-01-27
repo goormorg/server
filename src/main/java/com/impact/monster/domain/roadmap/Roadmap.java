6aa926fedc94d859e4dfdba670585c691a7e9457
hyeonjoonpark <pjjoon1379@gmail.com>
2024-01-27 23:26:09 +0900

    FEAT: roadmap 엔티티

---
+++ src/main/java/com/impact/monster/domain/roadmap/Roadmap.java
@@ -0,0 +1,34 @@
+package com.impact.monster.domain.roadmap;
+
+import com.impact.monster.domain.roadmap.types.StatusTypes.StatusType;
+import com.impact.monster.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity @Table(name = "roadmap")
+@Getter @ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Roadmap {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "roadmap_id")
+    private Long id;
+    private String info;
+    private String detail;
+    @Enumerated(EnumType.STRING)
+    private StatusType status;
+    @OneToMany
+    @JoinColumn(name = "user_id")
+    private List<User> userId;
+
+    @Builder
+    public Roadmap(Long id, String info, String detail, StatusType status, List<User> userId) {
+        this.id = id;
+        this.info = info;
+        this.detail = detail;
+        this.status = status;
+        this.userId = userId;
+    }
+}
